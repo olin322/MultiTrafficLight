8e768a882af94372c52ade17042e4b2460fa2b59
carla <olin@ford.com>
2023-10-27 17:19:57 +0800

    :D

--- MultiProcessing/check_model.py
+++ MultiProcessing/check_model.py
@@ -60,10 +60,10 @@ def checkModel_MultiPPO():
 	# 	verbose=1, 
 	# 	device='cuda'
 	# )
-	model = PPO.load(f"./models/0925/PPO_SeventeenTrafficLights_2048_3e-5_deltat_0.1_{140}e8[-2,2]")
+	model = PPO.load(f"./models/PPO_SeventeenTrafficLights_2048_3e-5_deltat_0.1_{200}e8[-2,2]")
 	eposides = 1
 
-	file_name = f"./check_result_log/0915_PPO_SeventeenTrafficLights_2048_3e-5_deltat_0.1_{140}e8[-2,2]"
+	file_name = f"./check_result_log/0915_PPO_SeventeenTrafficLights_2048_3e-5_deltat_0.1_{200}e8[-2,2]"
 	f = open(file_name, "a")
 	f.write("step, action, location, speed, observation\n")
 	data = ''
--- MultiProcessing/main.py
+++ MultiProcessing/main.py
@@ -1,9 +1,9 @@
 from functions import seventeenTrafficLights
 
-def main():
-	load_model_name = f"./models/PPO_SeventeenTrafficLights_2048_3e-5_deltat_0.1_{200}e8[-2,2]"
-	trained_model_name = f"./models/PPO_SeventeenTrafficLights_2048_3e-5_deltat_0.1_{220}e8[-2,2]"
-	training_iterations = 20e8
+def main(accumulated_epoc: int, epoc_this_run: int):
+	load_model_name = f"./models/PPO_SeventeenTrafficLights_2048_3e-5_deltat_0.1_{accumulated_epoc}e8[-2,2]"
+	trained_model_name = f"./models/PPO_SeventeenTrafficLights_2048_3e-5_deltat_0.1_{accumulated_epoc + epoc_this_run}e8[-2,2]"
+	training_iterations = epoc_this_run * 1e8
 	seventeenTrafficLights(load_model_name, trained_model_name, training_iterations)
 
-main()
+main(accumulated_epoc=220, epoc_this_run=20)
